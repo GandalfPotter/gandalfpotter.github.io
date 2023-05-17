a914808afc906089443aa76abc56d14028a857f8
root <root@huck.homenetwork>
2023-05-17 12:34:41 -0700

    wip list backups

--- js/showbak.js
+++ js/showbak.js
@@ -1,3 +1,17 @@
+function showBak(){
+	filetxt = getFileTxt();
+	files = filetxt.split("\n");
+	for(var i in files){
+		createLink(i, "/backups/"+i);
+	}
+}
+
+async function getFileTxt(){
+	const response = await fetch("/backups/FILES.txt");
+	const filetxt = await response.text();
+	return filetxt;
+}
+
 function createLink(name, addr){
 	var a = document.createElement('a');
 
